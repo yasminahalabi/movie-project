ebfd1ea6159d84fd7a8c4b28b4864f47d1b80e2d
yasminahalabi <yasminahalabi77@gmail.com>
2025-03-05 16:39:32 +0200

    added to docker

--- server/database.py
+++ server/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql://postgres:Pa55w.rd@localhost:5432/movies_db"
+DATABASE_URL = os.getenv("DATABASE_URL","postgresql://postgres:Pa55w.rd@db:5435/movies_db")
 
 engine = create_engine (DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
